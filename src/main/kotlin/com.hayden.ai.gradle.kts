4edfc5cb31f5cc3f1651361f5878dbe52ed8fe0e
hayden.rear <hayden.rear@gmail.com>
2025-02-14 15:46:23 -0500

    cmt

--- src/main/kotlin/com.hayden.ai.gradle.kts
+++ src/main/kotlin/com.hayden.ai.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
         .map { implementation(it) }
 //    implementation(vC.libs.vertexAi)
     runtimeOnly("ai.djl.pytorch:pytorch-native-cpu:2.0.1:osx-aarch64")
+    implementation("org.springframework.experimental:mcp:0.6.0")
 }
--- src/main/kotlin/com.hayden.docker-compose.gradle.kts
+++ src/main/kotlin/com.hayden.docker-compose.gradle.kts
@@ -5,5 +5,6 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-docker-compose")
     implementation("com.github.docker-java:docker-java:3.4.1")
+    implementation("com.github.docker-java:docker-java-transport-zerodep:3.4.1")
 }
 
