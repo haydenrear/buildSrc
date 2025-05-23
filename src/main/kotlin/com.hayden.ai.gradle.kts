3710a7dce5a10d016357b2a22006b82f964af85b
hayden.rear <hayden.rear@gmail.com>
2025-05-22 20:14:05 -0400

    add proper concurrent access of repositories through cloning additional

--- src/main/kotlin/com.hayden.ai.gradle.kts
+++ src/main/kotlin/com.hayden.ai.gradle.kts
@@ -1,5 +1,4 @@
 import com.hayden.haydenbomplugin.BuildSrcVersionCatalogCollector
-import gradle.kotlin.dsl.accessors._086de0b619b2f48ffb4e52cf6de7d048.implementation
 
 plugins {
     id("com.hayden.base-plugin")
@@ -10,7 +9,13 @@ val vC = project.extensions.getByType(BuildSrcVersionCatalogCollector::class)
 dependencies {
     vC.bundles.dglBundle.inBundle()
         .map { implementation(it) }
-//    implementation(vC.libs.vertexAi)
-    runtimeOnly("ai.djl.pytorch:pytorch-native-cpu:2.0.1:osx-aarch64")
+    vC.bundles.springAiClientBundle.inBundle()
+        .map { implementation(it) }
+    vC.bundles.springAiServerBundle.inBundle()
+        .map { implementation(it) }
+    vC.bundles.springAiBundle.inBundle()
+        .map { implementation(it) }
+
     implementation("com.fasterxml.jackson.module:jackson-module-jsonSchema:2.18.3")
+
 }
