b3a53cf5279ac36618c785deffc18e8cfaec07ca
hayden.rear <hayden.rear@gmail.com>
2025-05-31 15:38:48 -0400

    updated add proper concurrent access of repositories through cloning additional collect stream result add proper concurrent access of repositories through cloning additional collect stream result

--- src/main/kotlin/com.hayden.persistence.gradle.kts
+++ src/main/kotlin/com.hayden.persistence.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 dependencies {
     implementation("org.liquibase:liquibase-core")
     implementation("org.postgresql:postgresql")
-    testImplementation("com.h2database:h2")
     implementation("org.liquibase:liquibase-core")
 
     implementation("io.hypersistence:hypersistence-tsid:2.1.2")
