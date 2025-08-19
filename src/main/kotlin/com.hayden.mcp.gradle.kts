4e7189e6167c0e12e96c18a109929c5e54682a2f
hayden.rear <hayden.rear@gmail.com>
2025-08-18 20:40:57 -0400

    updating concurrency for indexing so that it works without stalling postgres

--- src/main/kotlin/com.hayden.mcp.gradle.kts
+++ src/main/kotlin/com.hayden.mcp.gradle.kts
@@ -2,18 +2,13 @@ import com.hayden.haydenbomplugin.BuildSrcVersionCatalogCollector
 
 plugins {
     id("com.hayden.base-plugin")
-    id("com.hayden.mcp")
 }
 
 val vC = project.extensions.getByType(BuildSrcVersionCatalogCollector::class)
 
 dependencies {
-    vC.bundles.dglBundle.inBundle()
+    vC.bundles.springAiClientBundle.inBundle()
         .map { implementation(it) }
-    vC.bundles.springAiBundle.inBundle()
+    vC.bundles.springAiServerBundle.inBundle()
         .map { implementation(it) }
-
-    implementation("com.fasterxml.jackson.module:jackson-module-jsonSchema:2.18.3")
-    implementation("org.springframework.ai:spring-ai-starter-model-ollama")
-
 }
