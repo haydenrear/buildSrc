f35859fe0fae2f82e8eea8c33282b0c461a305d4
hayden.rear <hayden.rear@gmail.com>
2025-09-30 19:24:36 -0400

    updated the otel dependencies and added micrometer to version catalog, updated limit and added tracing, updated and improved performance (everything sub-second)

--- src/main/kotlin/com.hayden.observable-lib.gradle.kts
+++ src/main/kotlin/com.hayden.observable-lib.gradle.kts
@@ -9,19 +9,17 @@ val vC = project.extensions.getByType(BuildSrcVersionCatalogCollector::class)
 dependencies {
     vC.bundles.opentelemetryBundle.inBundle()
         .map { implementation(it) }
+    vC.bundles.micrometerBundle.inBundle()
+        .map { implementation(it) }
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.opentelemetry:opentelemetry-api")
-    implementation("io.opentelemetry:opentelemetry-sdk")
-    implementation("io.opentelemetry:opentelemetry-exporter-logging")
 
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("io.micrometer:context-propagation:1.1.1")
-    runtimeOnly("io.micrometer:micrometer-core")
-    runtimeOnly("io.micrometer:micrometer-registry-otlp")
-    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+//    implementation("io.micrometer:context-propagation:1.1.1")
+//    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+//    implementation("io.micrometer:micrometer-registry-prometheus")
+//    runtimeOnly("io.micrometer:micrometer-core")
+//    runtimeOnly("io.micrometer:micrometer-registry-otlp")
+//    implementation("io.micrometer:micrometer-tracing-bridge-otel")
 
 }
 
