44f42985fd921b1ea5819c199393887581a6a9fd
hayden.rear <hayden.rear@gmail.com>
2025-09-30 12:55:52 -0400

    updated

---
+++ src/main/kotlin/com.hayden.suite.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.hayden.spring")
+}
+
+dependencies {
+    implementation("org.junit.platform:junit-platform-suite")
+    implementation("org.junit.jupiter:junit-jupiter-api")
+    implementation("org.junit.platform:junit-platform-suite-api:1.11.0")
+    implementation("org.assertj:assertj-core:3.26.3")
+}
