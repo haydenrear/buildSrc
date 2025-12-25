e42c36b4bf0177333190d61f44fe2dc76f5703b6
hayden.rear <hayden.rear@gmail.com>
2025-12-25 16:45:55 -0500

    removed dgs

--- src/main/kotlin/com.hayden.graphql.gradle.kts
+++ src/main/kotlin/com.hayden.graphql.gradle.kts
@@ -6,5 +6,4 @@ plugins {
 dependencies {
     testImplementation("org.springframework.graphql:spring-graphql-test")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
-    implementation("org.springframework.integration:spring-integration-graphql")
 }
