5348907b269bdb518315a71f114e95c99ec8c3dc
hayden.rear <hayden.rear@gmail.com>
2026-02-11 21:57:29 -0500

    incremented version embabel

--- src/main/kotlin/com.hayden.ai.gradle.kts
+++ src/main/kotlin/com.hayden.ai.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-jsonSchema:2.18.3")
     implementation("org.springframework.ai:spring-ai-starter-model-ollama")
     implementation("org.springframework.ai:spring-ai-starter-mcp-server-webmvc")
-    implementation("com.embabel.agent:embabel-agent-starter-openai:0.3.2-SNAPSHOT")
+    implementation("com.embabel.agent:embabel-agent-starter-openai:0.3.3-SNAPSHOT")
     implementation("com.agentclientprotocol:acp:0.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
     implementation("com.ag-ui.community:kotlin-core-jvm:0.2.4")
