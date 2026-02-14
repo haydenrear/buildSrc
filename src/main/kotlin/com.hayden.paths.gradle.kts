c1a58416235533acc2c34c03fbc8c8f0714b72e3
hayden.rear <hayden.rear@gmail.com>
2026-02-14 02:43:13 -0500

    set parent dir

--- src/main/kotlin/com.hayden.paths.gradle.kts
+++ src/main/kotlin/com.hayden.paths.gradle.kts
@@ -1,3 +1,38 @@
+tasks.register("processXmlFiles") {
+    group = "custom"
+    description = "Processes server.json and replaces environment variables"
+    dependsOn("processResources", "processTestResources")
+
+    fun doFile(value: String, buildDir: String) {
+        file(layout.projectDirectory).resolve(value)
+            .listFiles { it -> it.name.endsWith(".xml") }
+            ?.forEach {
+                println("Processing ${it.name}")
+                val inputPath = it
+                val outputPath = file(layout.buildDirectory).resolve("${buildDir}/${inputPath.name}")
+
+                if (!inputPath.exists()) {
+                    throw GradleException("File not found: ${inputPath.absolutePath}")
+                }
+
+                var originalContent = inputPath.readText()
+                originalContent = originalContent.replace("{{HOME_DIR}}", System.getenv().getOrDefault("HOME", "/Users/hayde"))
+                originalContent = originalContent.replace("{{PROJ_DIR}}", layout.projectDirectory.toString())
+                originalContent = originalContent.replace("{{PARENT_DIR}}", layout.settingsDirectory.toString())
+                originalContent = originalContent.replace("{{BUILD_DIR}}", file(layout.buildDirectory).toString())
+
+                outputPath.parentFile.mkdirs()
+                outputPath.writeText(originalContent)
+                println("Processed file written to: ${outputPath.absolutePath}")
+
+            }
+    }
+    doLast {
+        doFile("src/main/resources", "resources/main")
+        doFile("src/test/resources", "resources/test")
+    }
+}
+
 tasks.register("processYmlFiles") {
     group = "custom"
     description = "Processes server.json and replaces environment variables"
